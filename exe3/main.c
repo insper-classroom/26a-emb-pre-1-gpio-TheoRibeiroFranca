8f5b37fc0ad3f0a33fe6c6b0c20c09afe77f0b07
TheoRibeiroFranca <theorf@al.insper.edu.br>
2026-02-20 10:33:37 -0300

    exercicio 3 talvez funcionando

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,34 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_VERMELHO = 4;
+const int LED_VERDE = 6;
+const int BTN_VERMELHO = 28;
+const int BTN_VERDE = 26;
 int main() {
   stdio_init_all();
 
+  gpio_init(LED_VERMELHO);
+  gpio_init(LED_VERDE);
+  gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+  gpio_set_dir(LED_VERDE, GPIO_OUT);
+
+  gpio_init(BTN_VERMELHO);
+  gpio_set_dir(BTN_VERMELHO, GPIO_IN);
+  gpio_pull_up(BTN_VERMELHO);
+
+  gpio_init(BTN_VERDE);
+  gpio_set_dir(BTN_VERDE, GPIO_IN);
+  gpio_pull_up(BTN_VERDE);
+
   while (true) {
+    while(!gpio_get(BTN_VERMELHO)) {
+      gpio_put(LED_VERMELHO, 1);
+    }
+    gpio_put(LED_VERMELHO, 0);
+    while(!gpio_get(BTN_VERDE)) {
+      gpio_put(LED_VERDE, 1);
+    }
+    gpio_put(LED_VERDE, 0);
   }
 }
