20334f961303254ec4e2efc9cbf40618adf307f5
TheoRibeiroFranca <theorf@al.insper.edu.br>
2026-02-20 10:51:48 -0300

    exercicio 3 funcionando

--- exe3/main.c
+++ exe3/main.c
@@ -22,20 +22,27 @@ int main() {
   gpio_set_dir(BTN_VERDE, GPIO_IN);
   gpio_pull_up(BTN_VERDE);
 
+  int led_vermelho_estado = 0;
+  int led_verde_estado = 0;
+
+  gpio_put(LED_VERMELHO, led_vermelho_estado);
+  gpio_put(LED_VERDE, led_verde_estado);
+
   while (true) {
     if (gpio_get(BTN_VERMELHO) == 0) {
-      gpio_put(LED_VERMELHO, 1);
       while(!gpio_get(BTN_VERMELHO)) {
-        gpio_put(LED_VERMELHO, 1);
       }
-      gpio_put(LED_VERMELHO, 0);
+      led_vermelho_estado = !led_vermelho_estado;
+      gpio_put(LED_VERMELHO, led_vermelho_estado);
+      sleep_ms(20);
     }
+
     if (gpio_get(BTN_VERDE) == 0) {
-      gpio_put(LED_VERDE, 1);
       while(!gpio_get(BTN_VERDE)) {
-        gpio_put(LED_VERDE, 1);
       }
-      gpio_put(LED_VERDE, 0);
+      led_verde_estado = !led_verde_estado;
+      gpio_put(LED_VERDE, led_verde_estado);
+      sleep_ms(20);
     }
   }
 }
