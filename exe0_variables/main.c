c7b3699dfc692392da5c2518c318045b30640ac8
TheoRibeiroFranca <theorf@al.insper.edu.br>
2026-02-20 09:52:59 -0300

    exercicio0 correcao 3

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,15 +1,16 @@
 #include "stdio.h"
 
 
-void foo(void) {
-    a = a + 1; // acessa variavel global
+
+void foo(int *a) {
+    *a = *a + 1; // acessa variavel global
 }
 
 void main(void) {
     int a = 0;
     int b = 0;
     while (b < 2) {
-        foo();
+        foo(&a);
         
         if (a < 5) {
             b = 1;
