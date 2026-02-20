1fbdb1d3ef50e43016eff32a3bcca781b5fbbc69
TheoRibeiroFranca <theorf@al.insper.edu.br>
2026-02-20 10:25:28 -0300

    exercicio 2 funcionando

--- exe2/main.c
+++ exe2/main.c
@@ -2,19 +2,29 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN = 26;
+const int BTN_1_PIN = 26;
+const int BTN_2_PIN = 7;
+
 
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_PIN);
-  gpio_set_dir(BTN_PIN, GPIO_IN);
-  gpio_pull_up(BTN_PIN);
+  gpio_init(BTN_1_PIN);
+  gpio_set_dir(BTN_1_PIN, GPIO_IN);
+  gpio_pull_up(BTN_1_PIN);
+  gpio_init(BTN_2_PIN);
+  gpio_set_dir(BTN_2_PIN, GPIO_IN);
+  gpio_pull_up(BTN_2_PIN);
 
   while (true) {
-    if (!gpio_get(BTN_PIN)) {
+    if (!gpio_get(BTN_1_PIN)) {
       printf("Botao 1\n");
-      while (!gpio_get(BTN_PIN)) {
+      while (!gpio_get(BTN_1_PIN)) {
+      };
+    }
+    if (!gpio_get(BTN_2_PIN)) {
+      printf("Botao 2\n");
+      while (!gpio_get(BTN_2_PIN)) {
       };
     }
   }
